7f5e4f921f6c28889a47162591c13b2b05372eed
luislocon <luis.locon@gmail.com>
2017-10-03 10:09:46 -0600

    Modificando el servicio de imagen

--- client-album/src/app/services/image.service.ts
+++ client-album/src/app/services/image.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { GLOBAL } from './config';
+
+import { Image } from '../models/image.model';
 
 @Injectable()
 export class ImageService {
+  url: string;
 
-  constructor() { }
+  constructor(
+    private http: Http
+  ) {
+    this.url = GLOBAL.url;
+  }
 
 }
