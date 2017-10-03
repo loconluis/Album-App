3ff2702e2f569eed50cbe9a0d6359f7a6de1aafe
luislocon <luis.locon@gmail.com>
2017-10-03 11:11:21 -0600

    agregados los metodos al servicio de imagen

--- client-album/src/app/services/image.service.ts
+++ client-album/src/app/services/image.service.ts
@@ -17,6 +17,21 @@ export class ImageService {
     this.url = GLOBAL.url;
   }
 
+  getImages(albumID = null) {
+    if (albumID == null) {
+      return this.http.get(this.url + 'images/')
+        .map(res => res.json());
+    } else {
+      return this.http.get(this.url + 'images/' + albumID)
+      .map(res => res.json());
+    }
+  }
+
+  getImage(id) {
+    return this.http.get(this.url + 'image/' + id)
+      .map(res => res.json());
+  }
+
   saveImage(image: Image) {
     // guardar imagen
     const json = JSON.stringify(image);
@@ -25,6 +40,22 @@ export class ImageService {
 
     return this.http.post(this.url + 'image', params, { headers: headers })
       .map(res => res.json());
+  }
+
+  editImage(id: string, image: Image) {
+    // guardar imagen
+    const json = JSON.stringify(image);
+    const params = json;
+    const headers = new Headers({'Content-Type' : 'application/json'});
+
+    return this.http.put(this.url + 'image/' + id, params, { headers: headers })
+      .map(res => res.json());
+  }
+
+  deleteImage(id: string) {
+    return this.http.delete(this.url + 'image/' + id)
+      .map(res => res.json());
+    }
   }
 
 }
