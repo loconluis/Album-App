1fc844842f55034bfc98d7ccd328a8762f02b517
luislocon <luis.locon@gmail.com>
2017-10-04 17:47:23 -0600

    Peticion de imagenes de album terminado

--- client-album/src/app/album-detail/album-detail.component.ts
+++ client-album/src/app/album-detail/album-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { AlbumService } from '../services/album.service';
+import { ImageService } from '../services/image.service';
 import { Album } from '../models/album.model';
+import { Image } from '../models/image.model';
 
 @Component({
   selector: 'app-album-detail',
@@ -9,10 +11,12 @@ import { Album } from '../models/album.model';
 })
 export class AlbumDetailComponent implements OnInit {
   album: Album;
+  images: Array<Image>;
   errorMessage: any;
 
   constructor(
     private albumService: AlbumService,
+    private imageService: ImageService,
     private route: ActivatedRoute,
     private router: Router
   ) { }
@@ -36,13 +40,37 @@ export class AlbumDetailComponent implements OnInit {
             this.router.navigate(['/']);
           }
 
-        }, err => {
+          // llamada a las imagenes del album
+          this.getImagesOnAlbum(response.album._id);
+
+        }, err => { // error del metodo que trae los album
           this.errorMessage = <any>err;
           if (this.errorMessage != null) {
             console.log(this.errorMessage);
             alert('Error en la peticion');
           }
         });
+    });
+  }
+
+  getImagesOnAlbum(id: string) {
+    // llamada al servicio de imagenes
+    this.imageService.getImages(id)
+    .subscribe(result => {
+      // exito
+      // console.log(result);
+      this.images = result.images;
+      // si no existen las imagenes
+      if (!this.images) {
+        alert('Sin imagenes');
+      }
+
+    }, err => {
+      this.errorMessage = <any>err;
+      if (this.errorMessage != null) {
+        console.log(this.errorMessage);
+        alert('Error en la peticion');
+      }
     });
   }
 
